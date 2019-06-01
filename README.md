2d883a42272f95c196859ab1be70391dce9bf875
Frederik Reiter <kontakt@frederikreiter.de>
2019-06-01 17:31:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Common Misconceptions
+
+This Flask App uses the Wikipedia page [List of common misconceptions](https://en.wikipedia.org/wiki/List_of_common_misconceptions) and shows a random one to the user.
---
+++ app.py
@@ -0,0 +1,39 @@
+import json
+import random
+import wikipedia
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+misconceptions = []
+
+
+def get_all_misconceptions() -> [str]:
+    article = wikipedia.page("list_of_common_misconceptions")
+    content = article.content.split("== See also ==")[0]  # Exclude references etc
+    parsed = []
+    for line in content.split("\n"):
+        if not line.startswith("=") and len(line.strip()) != 0:  # not a title and not empty
+            parsed.append(line)
+    return parsed[1:]  # remove the descriptor line
+
+
+@app.before_first_request
+def load_misconceptions():
+    global misconceptions
+    misconceptions = get_all_misconceptions()
+
+
+@app.route('/')
+def random_misconception():
+    return render_template("misconception.html", misconception=random.choice(misconceptions))
+
+
+@app.route('/random')
+def random_misconception_raw():
+    return random.choice(misconceptions)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
